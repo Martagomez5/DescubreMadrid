37da6762eba3bf19f95fd79724faf956498421a9
marta052000 <76521905+marta052000@users.noreply.github.com>
2021-01-28 20:29:01 +0100

    Creacion de la interfaz de registro

--- app/src/main/java/com/example/descubremadrid/Login.java
+++ app/src/main/java/com/example/descubremadrid/Login.java
@@ -2,13 +2,84 @@ package com.example.descubremadrid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
+  EditText textcorreo,textcontra;
+  Button btnlog;
+  String correo,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textcorreo=findViewById(R.id.email_iniciosesion);
+        textcontra=findViewById(R.id.contraseña_iniciosesion);
+        btnlog=findViewById(R.id.btn_iniciosesion);
+
+        btnlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                correo=textcorreo.getText().toString();
+                pass=textcontra.getText().toString();
+                if(!correo.isEmpty() && !pass.isEmpty()){
+                    validarUsuario("http://213.37.147.60:80/proyecto/validarusuario.php");
+
+                }
+                else{
+                    Toast.makeText(Login.this, "No se permiten campos vacios", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private void validarUsuario(String URL){
+        StringRequest stringRequest= new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(!response.isEmpty()){
+                    Intent intent = new Intent(getApplicationContext(), Principal.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Login.this, "Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Login.this, error.toString(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros=new HashMap<String, String>();
+                parametros.put("correo",textcorreo.getText().toString());
+                parametros.put("contrasena",textcontra.getText().toString());
+
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
---
+++ app/src/main/java/com/example/descubremadrid/Lugar.java
@@ -0,0 +1,14 @@
+package com.example.descubremadrid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lugar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lugar);
+    }
+}
