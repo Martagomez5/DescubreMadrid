62dc96c326de5c6251038dc6f3ac3bd8da5e4e57
marta052000 <76521905+marta052000@users.noreply.github.com>
2021-05-31 16:03:49 +0200

    Terminado proyecto

--- app/src/main/java/com/example/descubremadrid/DetallesLugares.java
+++ app/src/main/java/com/example/descubremadrid/DetallesLugares.java
@@ -1,16 +1,22 @@
 package com.example.descubremadrid;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.fonts.Font;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -41,12 +47,12 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
     TextView tvNombre, tvtipo, tvhorario, tvweb, tvprecio, tvdescuento, tvreserva, tvtelefono, tvubicacion, tvtransporte, tvaccesibilidad, tvtiempoVisita;
     ImageView tvdireccion;
     Button btnTelefono, btnWeb, buttonQV;
-    CheckBox cb;
+    CheckBox cb, visto;
     int tiempoEnHoras;
 
 
     public static String respuesta;
-
+    public static String respuesta2;
     Context context;
     public static String id2;
     public static String idPersona;
@@ -54,6 +60,7 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
     RequestQueue request;
     JsonObjectRequest jsonObjectRequest2;
     RequestQueue request2;
+    private static final int REQUEST_PERMISSION_CALL = 100;
 
 
 
@@ -83,6 +90,7 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
         tvtiempoVisita = findViewById(R.id.textViewTiempoVisita);
         cb=findViewById(R.id.checkBoxQuererVer);
         buttonQV=findViewById(R.id.buttonGuardar);
+        visto=findViewById(R.id.visto);
 
 
 
@@ -102,6 +110,8 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
         cargarWeb();
 
 
+
+
         btnWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -114,10 +124,25 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
         btnTelefono.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+tvtelefono.getText().toString()));
-                if (ActivityCompat.checkSelfPermission(DetallesLugares.this, Manifest.permission.CALL_PHONE)!=
-                        PackageManager.PERMISSION_GRANTED)return;
-                    startActivity(intent2);
+
+                if (Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+                    call(tvtelefono.getText().toString());
+                }else{
+                    Log.i("TAG", "API >=23");
+                    if (ContextCompat.checkSelfPermission(DetallesLugares.this, Manifest.permission.CALL_PHONE)==PackageManager.PERMISSION_GRANTED){
+                        Log.i("TAG", "Permiso concedido");
+                        call(tvtelefono.getText().toString());
+                    }else{
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(DetallesLugares.this, Manifest.permission.CALL_PHONE)){
+                            btnTelefono.setEnabled(false);
+                            Log.i("TAG", "Usuario ha rechazado el permiso");
+
+                        }else {
+                            Log.i("TAG", "Permiso concedido");
+                        }
+                        ActivityCompat.requestPermissions(DetallesLugares.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PERMISSION_CALL);
+                    }
+                }
 
 
             }
@@ -128,12 +153,29 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
             public void onClick(View v) {
 
                 String URL="https://descubremadrid.xyz/descubreMadrid/quererVerGuardar.php";
+                String URL2="https://descubremadrid.xyz/descubreMadrid/vistoGuardar.php";
                 if(cb.isChecked()==true){
                     respuesta="1";
                 }else{
                     respuesta="0";
                 }
                 guardar(URL);
+                guardar2(URL2);
+
+            }
+        });
+
+        visto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String URL2="https://descubremadrid.xyz/descubreMadrid/vistoGuardar.php";
+                if(visto.isChecked()==true){
+                    respuesta2="1";
+                }else{
+                    respuesta2="0";
+                }
+                //guardar2(URL2);
 
             }
         });
@@ -179,7 +221,7 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
             dLugares.setUbicacion(jsonObject.optString("ubicacion"));
             dLugares.setTransporte(jsonObject.optString("transporte"));
             dLugares.setAccesibilidad(jsonObject.optString("accesibilidad"));
-            dLugares.setTiempoVisita(jsonObject.optString("tiempoVisita"));
+            dLugares.setTiempoVisita(jsonObject.optDouble("tiempoVisita"));
 
 
         } catch (JSONException e) {
@@ -198,7 +240,8 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
         tvubicacion.setText(Html.fromHtml("<FONT COLOR='black'><b>Ubicación:  </b></FONT>"+ dLugares.getUbicacion()));
         tvtransporte.setText(Html.fromHtml("<FONT COLOR='black'><b>Transporte:  </b></FONT>"+ dLugares.getTransporte()));
         tvaccesibilidad.setText(Html.fromHtml("<FONT COLOR='black'><b>Accesibilidad:  </b></FONT>"+ dLugares.getAccesibilidad()));
-        tvtiempoVisita.setText(Html.fromHtml("<FONT COLOR='black'><b>Tiempo duración visita:  </b></FONT>"+ dLugares.getTiempoVisita()));
+
+        tvtiempoVisita.setText(Html.fromHtml("<FONT COLOR='black'><b>Tiempo duración visita:  </b></FONT>"+ String.valueOf( String.format("%.2f", dLugares.getTiempoVisita()/60))+"h"));
 
          if(dLugares.getDescuento().equals("null")){
              tvdescuento.setHeight(0);
@@ -227,6 +270,7 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
 
 
         querer();
+        visto();
 
 
 
@@ -269,6 +313,83 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
                 requestQueue.add(stringRequest);
     }
 
+    public void visto(){
+
+        String URL="https://descubremadrid.xyz/descubreMadrid/pp2.php?idLugar="+id2.toString() + "&idPersona="+idPersona.toString();
+
+        StringRequest stringRequest= new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                respuesta2=response.trim();
+                if(respuesta2.equals("0")){
+                    respuesta2="0";
+                    visto.setChecked(false);
+
+                }else if(respuesta2.equals("1")){
+                    respuesta2="1";
+                    visto.setChecked(true);
+
+                }else{
+                    respuesta2="0";
+                    visto.setChecked(false);
+
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+    private void guardar2(String URL) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+
+
+                        //Toast.makeText(getApplicationContext(), "GUARDADO ", Toast.LENGTH_SHORT).show();
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String t = "GUARDADO INCORRECTO";
+                Toast.makeText(getApplicationContext(), t, Toast.LENGTH_SHORT).show();
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                if(respuesta2.equals("0")){
+
+                    respuesta2="0";
+                }else {
+                    respuesta2="1";
+                }
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("visto", respuesta2);
+                parametros.put("idPersona", idPersona);
+                parametros.put("idLugar", id2);
+                return parametros;
+            }
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
     private void guardar(String URL) {
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
@@ -278,7 +399,7 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
 
 
 
-                        Toast.makeText(getApplicationContext(), "GUARDADO "+respuesta, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "GUARDADO ", Toast.LENGTH_SHORT).show();
 
                     }
                 }, new Response.ErrorListener() {
@@ -309,10 +430,54 @@ public class DetallesLugares extends AppCompatActivity implements  Response.List
         requestQueue.add(stringRequest);
     }
 
-public void visibilidad(){
+
+    public void visibilidad(){
     if(tvdescuento.getText().equals("null")){
         tvdescuento.setVisibility(View.INVISIBLE);
     }
 }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION_CALL){
+            if (permissions.length >0 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                btnTelefono.setEnabled(false);
+            }
+
+            if (permissions.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Log.i("TAG", "Permiso permitido");
+                btnTelefono.setEnabled(true);
+                call(tvtelefono.getText().toString());
+            }else {
+                Log.i("TAG", "Permiso denegado" );
+                if (ActivityCompat.shouldShowRequestPermissionRationale(DetallesLugares.this, Manifest.permission.CALL_PHONE)){
+                    new AlertDialog.Builder(this).setMessage("Necesitas aceptar el permiso para poder llamar")
+                            .setPositiveButton("Intentalo de Nuevo", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ActivityCompat.requestPermissions(DetallesLugares.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_PERMISSION_CALL);
+                                }
+                            })
+                            .setNegativeButton("No Gracias", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    btnTelefono.setEnabled(false);
+                                    Log.i("TAG", "Dejado" );
+                                }
+                            }).show();
+                }
+            }
+
+        }
+
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void call(String telefono) {
+        Intent intent2 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + tvtelefono.getText().toString()));
+        if (ActivityCompat.checkSelfPermission(DetallesLugares.this, Manifest.permission.CALL_PHONE)!=
+                PackageManager.PERMISSION_GRANTED)return;
+        startActivity(intent2);
+    }
 
 }
